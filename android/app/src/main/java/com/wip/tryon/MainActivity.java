88ff3b0b6a99f4f5bb52cc8e3734af5ef7ab844c
Nikita <rioskill@yandex.ru>
2024-04-15 23:29:47 +0300

    apk building set

--- android/app/src/main/java/com/wip/tryon/MainActivity.java
+++ android/app/src/main/java/com/wip/tryon/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativeapp;
+package com.wip.tryon;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -14,7 +14,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "reactNativeApp";
+    return "TryOn";
   }
 
   /**
--- android/app/src/main/java/com/wip/tryon/MainApplication.java
+++ android/app/src/main/java/com/wip/tryon/MainApplication.java
@@ -1,4 +1,4 @@
-package com.reactnativeapp;
+package com.wip.tryon;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
