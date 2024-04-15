bc036d39f182113283ce7eb2f51bdf8e18bc2400
nikstarling <rioskill@yandex.ru>
2024-04-15 15:08:58 +0300

    centrifuge connection refactor

--- src/requests/centrifuge.ts
+++ src/requests/centrifuge.ts
@@ -1,4 +1,4 @@
-import { Centrifuge } from "centrifuge";
+import { Centrifuge, PublicationContext } from "centrifuge";
 import { apiEndpoint, centrifugeEndpoint, login, password, staticEndpoint } from "../../config";
 import { appState } from "../stores/AppState";
 import { garmentStore } from "../stores/GarmentStore";
@@ -6,6 +6,36 @@ import { resultStore } from "../store";
 import { runInAction } from "mobx";
 import { outfitGenResutlStore, outfitGenUUIDStore } from "../stores/OutfitGenStores";
 
+interface CentrifugeSubscriptionProps {
+    connection: Centrifuge
+    name: string,
+    onPublication: (ctx: PublicationContext)=>void
+}
+
+const subsribeToChannel = (props: CentrifugeSubscriptionProps) => {
+    const channel = props.connection.newSubscription(props.name);
+
+    channel.on('subscribing', (ctx) => {
+        console.log(`subscribing to ${props.name}`);
+    });
+  
+    channel.on('subscribed', function(ctx) {
+        console.log(`subscribed to ${props.name}`);
+    });
+  
+    channel.on('unsubscribed', function(ctx) {
+        console.log(`unsubscribed from ${props.name}`);
+    });
+
+    channel.on('error', ctx => {
+        console.log(`error in ${props.name}`, ctx);
+    })
+
+    channel.on('publication', props.onPublication);
+
+    channel.subscribe();
+    return channel;
+}
 
 export const loginFunc = async () => {
     const loginBody = {
@@ -48,128 +78,87 @@ export const loginFunc = async () => {
         console.log('disconnected', ctx);
     });
     
-    const processing_sub = centrifuge.newSubscription(`processing:user#${json.user_id}`);
-    const try_on_sub = centrifuge.newSubscription(`try-on:user#${json.user_id}`);
+    const processing_sub = subsribeToChannel({
+        connection: centrifuge,
+        name: `processing:user#${json.user_id}`,
+        onPublication: ctx => {
+            console.log('processing', ctx.data);
 
-    const outfit_gen = centrifuge.newSubscription(`outfit-gen:user#${json.user_id}`);
-
-    outfit_gen.on('subscribing', function(ctx) {
-        console.log('subscribing to outfit gen');
-    });
-  
-    outfit_gen.on('subscribed', function(ctx) {
-        console.log('subscribed to outfit gen');
-    });
-  
-    outfit_gen.on('unsubscribed', function(ctx) {
-        console.log('unsubscribed from outfit gen');
-    });
-
-    outfit_gen.on('publication', function(ctx) {
-        console.log(ctx);
-
-        const outfits = ctx.data.outfits.map((outfit: {clothes: {clothes_id: string}[]}) => 
-            outfit.clothes.map(c => c.clothes_id))
-        
-        outfitGenUUIDStore.setOutfits(outfits);
-    })
-
-    outfit_gen.on('error', ctx => {
-        console.log('error', ctx);
-    })
-
-    processing_sub.on('subscribing', function(ctx) {
-        console.log('subscribing to processing');
-    });
-  
-    processing_sub.on('subscribed', function(ctx) {
-        console.log('subscribed to processing');
-    });
-  
-    processing_sub.on('unsubscribed', function(ctx) {
-        console.log('unsubscribed from porcessing');
-    });
-  
-    try_on_sub.on('subscribing', function(ctx) {
-        console.log('subscribing to try on');
-    });
-  
-    try_on_sub.on('subscribed', function(ctx) {
-        console.log('subscribed to try on');
-    });
-  
-    try_on_sub.on('unsubscribed', function(ctx) {
-        console.log('unsubscribed from try on');
-    });
-  
-    processing_sub.on('publication', function(ctx) {
-        console.log('processing', ctx.data);
-
-        const garment = garmentStore.getGarmentByUUID(ctx.data.uuid);
-
-        if (garment === undefined) {
-            console.error(`garment with uuid ${ctx.data.uuid} does not exist`);
-            return false;
+            const garment = garmentStore.getGarmentByUUID(ctx.data.uuid);
+    
+            if (garment === undefined) {
+                console.error(`garment with uuid ${ctx.data.uuid} does not exist`);
+                return false;
+            }
+    
+            const type = garmentStore.getTypeByUUID(ctx.data.classification.type);
+            const subtype = garmentStore.getSubTypeByUUID(ctx.data.classification.subtype);
+            const style = garmentStore.getStyleByUUID(ctx.data.classification.style);
+            const tags = ctx.data.classification.tags;
+            const seasons = ctx.data.classification.seasons;
+            
+            
+            runInAction(() => {
+                garment.image = {
+                    type: 'remote',
+                    uri: ctx.data.image
+                }
+    
+                if (type === undefined) {
+                    console.log(`type with uuid ${ctx.data.classification.type} does not exist`)
+                } else {
+                    // garment.setType(type);
+                    garment.type = type;
+                }
+    
+                if (subtype === undefined) {
+                    console.log(`subtype with uuid ${ctx.data.classification.subtype} does not exist`)
+                } else {
+                    // garment.setSubtype(subtype);
+                    garment.subtype = subtype;
+                }
+    
+                if (style === undefined) {
+                    console.log(`style with uuid ${ctx.data.classification.style} does not exist`)
+                } else {
+                    // garment.setStyle(style);
+                    garment.style = style;
+                }
+                // garment.setTags(tags);
+                // garment.setSeasons(seasons);
+                garment.tags = tags;
+                garment.seasons = seasons;
+                garment.tryOnAble = ctx.data.tryonable;
+            })
+    
+            console.log('resulting garment:', garment)
         }
-
-        const type = garmentStore.getTypeByUUID(ctx.data.classification.type);
-        const subtype = garmentStore.getSubTypeByUUID(ctx.data.classification.subtype);
-        const style = garmentStore.getStyleByUUID(ctx.data.classification.style);
-        const tags = ctx.data.classification.tags;
-        const seasons = ctx.data.classification.seasons;
-        
-        
-        runInAction(() => {
-            garment.image = {
-                type: 'remote',
-                uri: ctx.data.image
-            }
-
-            if (type === undefined) {
-                console.log(`type with uuid ${ctx.data.classification.type} does not exist`)
-            } else {
-                // garment.setType(type);
-                garment.type = type;
-            }
-
-            if (subtype === undefined) {
-                console.log(`subtype with uuid ${ctx.data.classification.subtype} does not exist`)
-            } else {
-                // garment.setSubtype(subtype);
-                garment.subtype = subtype;
-            }
-
-            if (style === undefined) {
-                console.log(`style with uuid ${ctx.data.classification.style} does not exist`)
-            } else {
-                // garment.setStyle(style);
-                garment.style = style;
-            }
-            // garment.setTags(tags);
-            // garment.setSeasons(seasons);
-            garment.tags = tags;
-            garment.seasons = seasons;
-            garment.tryOnAble = ctx.data.tryonable;
-        })
-
-        console.log('resulting garment:', garment)
     });
-    
-    try_on_sub.on('publication', function(ctx) {
-        console.log(ctx.data);
-        console.log(staticEndpoint + ctx.data.image)
 
-        resultStore.setResultUrl(staticEndpoint + ctx.data.image);
-        resultStore.setResultUUID(ctx.data.uuid);
+    const try_on_sub = subsribeToChannel({
+        connection: centrifuge,
+        name: `try-on:user#${json.user_id}`,
+        onPublication: ctx => {
+            console.log(ctx.data);
+            console.log(staticEndpoint + ctx.data.image)
+
+            resultStore.setResultUrl(staticEndpoint + ctx.data.image);
+            resultStore.setResultUUID(ctx.data.uuid);
+        }
     });
-  
-    processing_sub.on('error', function(ctx) {
-      console.log("subscription error", ctx);
+
+    const outfit_gen = subsribeToChannel({
+        connection: centrifuge,
+        name: `outfit-gen:user#${json.user_id}`,
+        onPublication: ctx => {
+            console.log(ctx);
+
+            const outfits = ctx.data.outfits.map((outfit: {clothes: {clothes_id: string}[]}) => 
+                outfit.clothes.map(c => c.clothes_id))
+            
+            outfitGenUUIDStore.setOutfits(outfits);
+        }
     });
-    
-    processing_sub.subscribe();
-    try_on_sub.subscribe();
-    outfit_gen.subscribe();
     
     centrifuge.connect();
 }
